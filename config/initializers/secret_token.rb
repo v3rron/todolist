b19be5f80b88c69c1f563a882b715cda5e1a2605
Ronen Verdi <go7hic@gmail.com>
2013-09-04 16:15:46 -0400

    Initial commit
    Gems added: bootstrap, backbone-on-rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Todolist::Application.config.secret_key_base = '177c75a15196b6fb416806f5a61477a28e55964cca8947e7431cf2d4a26610224fd2594d092e7a85fa799fba4d94ad4c86a10dc91b45ed6d771315cef7faf516'
